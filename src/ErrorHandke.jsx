81d1d2028161de5622e77a98e770e989eb7e1671
muhammad haseen <hasinmmpm786@gmail.com>
2024-09-01 07:15:08 +0530

    error handle update

--- src/ErrorHandke.jsx
+++ src/ErrorHandke.jsx
@@ -7,7 +7,7 @@ export default function ErrorPage() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-secondary gap-10 text-black">
       <h1 className="text-3xl">Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p className="text-2xl">Sorry, an unexpected error has occurred.</p>
       <p className="text-6xl">
         <i >{error.statusText || error.message}</i>
       </p>
