b547c23ca0917e32547c60a1dd219142ac946f9d
muhammad haseen <hasinmmpm786@gmail.com>
2024-10-08 15:37:19 +0530

    chabnged fixed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ import { Context } from './Global/ContextList';
 const Footer = () => {
     const {  role,id } = useContext(Context);
   return (
-    <main className='flex flex-col md:flex-row justify-between items-center fixed bottom-0 bg-primary w-full text-sm md:text-xl py-2'>
+    <main className='flex flex-col md:flex-row justify-between items-center  bg-primary w-full text-sm md:text-xl py-2'>
         
         <p className='text-secondary'>User ID: <span className='text-white'>{id}</span> </p>
    
